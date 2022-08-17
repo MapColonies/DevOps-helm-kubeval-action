d415314d8cece264741422f7e5abac2fe84e7d5f
SAPIROD <sapirod@ubuntu2004lww1313.WWEST.LOCAL>
2022-08-17 14:56:34 +0300

    changes for added schema url, no config file and single chart support

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,24 +23,25 @@ run_kubeval() {
     rm -rf helm-output;
 }
 
-# Parse helm repos located in the $CONFIG_FILE file / Ignore commented lines (#)
-while read REPO_CONFIG
-do
-    case "$REPO_CONFIG" in \#*) continue ;; esac
-    REPO=$(echo $REPO_CONFIG | cut -d '=' -f1);
-    URL=$(echo $REPO_CONFIG | cut -d '=' -f2);
-    helm repo add $REPO $URL;
-done < $CONFIG_FILE
+if [[ ! -z "$CONFIG_FILE" ]]; then
+    # Parse helm repos located in the $CONFIG_FILE file / Ignore commented lines (#)
+    while read REPO_CONFIG
+    do
+        case "$REPO_CONFIG" in \#*) continue ;; esac
+        REPO=$(echo $REPO_CONFIG | cut -d '=' -f1);
+        URL=$(echo $REPO_CONFIG | cut -d '=' -f2);
+        helm repo add $REPO $URL;
+    done < $CONFIG_FILE
+fi
 
-# For all charts (i.e for every directory) in the directory
-for CHART in "$CHARTS_PATH"/*/; do
-    echo "Validating $CHART Helm Chart...";
-    cd "$CURRENT_DIR/$CHART";
+validate_chart() {
+    echo "Validating $1 Helm Chart...";
+    cd "$CURRENT_DIR/$1";
     helm dependency build;
 
     if [ -d "ci" ]; then
         for VALUES_FILE in ci/*-values.yaml; do
-            echo "Validating $CHART Helm Chart using $VALUES_FILE values file...";
+            echo "Validating $1 Helm Chart using $VALUES_FILE values file...";
             run_kubeval "$(pwd)" "$VALUES_FILE"
         done
     else
@@ -48,4 +49,13 @@ for CHART in "$CHARTS_PATH"/*/; do
     fi
     echo "Cleanup $(pwd)/charts directory after we are done running Kubeval"
     rm -rf $(pwd)/charts/
-done
+}
+
+if test -f "$CHARTS_PATH/Chart.yaml"; then
+	validate_chart $CHARTS_PATH
+else
+    # For all charts (i.e for every directory) in the directory
+    for CHART in "$CHARTS_PATH"/*/; do
+        validate_chart $CHART
+    done
+fi
